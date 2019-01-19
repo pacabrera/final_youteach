2febc8fad6694f521ef57c8ec351c35a52912aca
paulandreycabrera <paulandreycabrera@gmail.com>
2019-01-19 17:10:29 +0800

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
     <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/popper.js/umd/popper.min.js') }}"> </script>
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/jquery.cookie/jquery.cookie.js') }}"> </script>>
+    <script src="{{ asset('assets/vendor/jquery.cookie/jquery.cookie.js') }}"> </script>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
     <script src="{{ asset('assets/js/front.js') }}"></script>
 
