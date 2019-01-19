34d9f69421200263fa742891dd81b62a183184ca
paulandreycabrera <paulandreycabrera@gmail.com>
2019-01-19 17:09:20 +0800

    fix

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -68,92 +68,9 @@
                   </div>
                 </div>
 
-                <div class="card mb-3">
-                  <div class="card-header">
-                    <h2 class="h6 text-uppercase mb-0">Bar chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <div class="chart-holder">
-                      <canvas id="barChartExample"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-            <div class="row mb-4">
-              <div class="col-lg-8">
-                <div class="card">
-                  <div class="card-header">
-                    <h2 class="h6 mb-0 text-uppercase">Bar chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <p class="mb-5 text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                    <div class="chart-holder mt-5 mb-5">
-                      <canvas id="barChartExample1"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="card mb-4">
-                  <div class="card-header">
-                    <h2 class="h6 text-uppercase mb-0">Doughnut chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <p class="mb-3 text-gray">Lorem ipsum dolor sit amet.</p>
-                    <div class="chart-holder">
-                      <canvas id="doughnutChartExample"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="card mb-4">
-                  <div class="card-header">
-                    <h2 class="h6 text-uppercase mb-0">Pie chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <p class="mb-3 text-gray">Lorem ipsum dolor sit amet.</p>
-                    <div class="chart-holder">
-                      <canvas id="pieChartExample"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="card mb-4">
-                  <div class="card-header">
-                    <h2 class="h6 text-uppercase mb-0">Polar chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <p class="mb-3 text-gray">Lorem ipsum dolor sit amet.</p>
-                    <div class="chart-holder">
-                      <canvas id="polarChartExample"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="card mb-4">
-                  <div class="card-header">
-                    <h2 class="h6 text-uppercase mb-0">Radar chart Example</h2>
-                  </div>
-                  <div class="card-body">
-                    <p class="mb-3 text-gray">Lorem ipsum dolor sit amet.</p>
-                    <div class="chart-holder">
-                      <canvas id="radarChartExample"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </section>
         </div>
-        <footer class="footer bg-white shadow
+        
 
 @endsection
