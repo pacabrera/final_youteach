11d63f738879c95dc5f465defaafe094433c1375
paulandreycabrera <paulandreycabrera@gmail.com>
2019-01-25 12:53:41 +0800

    comment validation

--- resources/views/teacher/post.blade.php
+++ resources/views/teacher/post.blade.php
@@ -86,10 +86,10 @@
                                             <textarea id="my-editor" name="comment" class="form-control" {{ $errors->has('comment') ? 'is-invalid' : '' }}></textarea>
                                                             <div class="invalid-feedback">
                     {{ $errors->first('comment') }}
-                </div
+                </div>
                                       </div>
                               
->
+
                               <div class="btn-toolbar justify-content-between" style="margin-top: 25px;">
                                   <div class="btn-group">
                                       <button type="submit" class="btn btn-primary" class="btn">Post</button>
